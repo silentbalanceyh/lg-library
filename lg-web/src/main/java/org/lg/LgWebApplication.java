6d921f2884bdd30d76d172ceea0c718d0d7a632e
Lang <silentbalanceyh@126.com>
2017-05-04 11:12:27 +0800

    Tradeshift Explorer Project

---
+++ lg-web/src/main/java/org/lg/LgWebApplication.java
@@ -0,0 +1,12 @@
+package org.lg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LgWebApplication.class, args);
+	}
+}
