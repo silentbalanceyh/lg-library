c74e76980526ad4a742c16993b10a94abf84214c
Lang <silentbalanceyh@126.com>
2017-05-09 10:09:10 +0800

    Normalizer batch testing tool finished.

--- lg-babelway/lg-babelway-api/src/main/java/org/lg/babelway/test/UnitNormalizer.java
+++ lg-babelway/lg-babelway-api/src/main/java/org/lg/babelway/test/UnitNormalizer.java
@@ -3,7 +3,6 @@ package org.lg.babelway.test;
 import org.lg.babelway.test.cv.TestConstant;
 import org.lg.ce.json.JsonObject;
 import org.lg.ce.model.ParamsMap;
-import org.lg.cv.Files;
 import org.lg.cv.Symbols;
 import org.lg.facade.Normalizer;
 import org.lg.hors.AbstractException;
@@ -13,11 +12,11 @@ import org.lg.util.Instance;
 public class UnitNormalizer {
 	
 	private static String getDataPath(final String filename){
-		return TestConstant.Folder.DATA_NORMALIZE + Symbols.SLASH + filename + Files.Ext.DOT_JSON;
+		return TestConstant.Folder.DATA_NORMALIZE + Symbols.SLASH + filename;
 	}
 	/** Test Shared Tool for normalizer **/
-	public static void testNormalizer(final Class<?> normalizeCls, final String filename) throws AbstractException {
-		final Normalizer normalizer = Instance.singleton(normalizeCls);
+	public static void testNormalizer(final Class<?> normalizerCls, final String filename) throws AbstractException {
+		final Normalizer normalizer = Instance.singleton(normalizerCls);
 		final JsonObject input = IOKit.getJObject(getDataPath(filename));
 		final ParamsMap params = new ParamsMap(input);
 		normalizer.normalize(params);
--- lg-babelway/lg-babelway-api/src/test/java/org/lg/babelway/normalize/TicketNormalizerTest.java
+++ lg-babelway/lg-babelway-api/src/test/java/org/lg/babelway/normalize/TicketNormalizerTest.java
@@ -1,36 +1,102 @@
 package org.lg.babelway.normalize;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.lg.babelway.hors.NormalizerException;
 import org.lg.babelway.test.UnitNormalizer;
+import org.lg.babelway.test.cv.TestConstant;
+import org.lg.cv.Defaults;
+import org.lg.cv.Symbols;
 import org.lg.hors.AbstractException;
+import org.lg.log.Log;
+import org.lg.util.IOKit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TicketNormalizerTest {
-	// action = /ticket
-	@Test(expected = NormalizerException.class)
-	public void testErrTicket() throws AbstractException {
-		UnitNormalizer.testNormalizer(TicketNormalizer.class, "ticket-key-failure");
+	private static final Logger LOGGER = LoggerFactory.getLogger(TicketNormalizerTest.class);
+	private static ConcurrentMap<String, Class<?>> NORMALIZER = new ConcurrentHashMap<>();
+
+	@BeforeClass
+	public static void setUp() {
+		NORMALIZER.put("ticket", TicketNormalizer.class);
+		NORMALIZER.put("tickets", TicketsNormalizer.class);
+		NORMALIZER.put("deleteTicket", DeleteTicketNormalizer.class);
 	}
+
+	private Class<?> getNormalizer(final String filename) {
+		final String key = filename.split(Symbols.DASH)[Defaults.INDEX];
+		return NORMALIZER.get(key);
+	}
+
+	private int countByFlag(final String flag) {
+		final List<String> files = IOKit.listFiles(TestConstant.Folder.DATA_NORMALIZE);
+		int counter = Defaults.ZERO;
+		for (final String file : files) {
+			if (Defaults.ZERO <= file.indexOf(flag)) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+
+	private void debugOutput(final String flag, final String file, final boolean isCommon) {
+		if (isCommon) {
+			if ("success".equals(flag)) {
+				Log.info(LOGGER, "Expected Success: " + file);
+			} else {
+				Log.warn(LOGGER, "Expected Success: " + file);
+			}
+		} else {
+			if ("success".equals(flag)) {
+				Log.warn(LOGGER, "Expected Failure: " + file);
+			} else {
+				Log.info(LOGGER, "Expected Failure: " + file);
+			}
+		}
+	}
+
+	private List<NormalizerException> execute(final String flag) throws AbstractException {
+		final List<String> files = IOKit.listFiles(TestConstant.Folder.DATA_NORMALIZE);
+		List<NormalizerException> errors = new ArrayList<>();
+		for (final String file : files) {
+			if (Defaults.ZERO <= file.indexOf(flag)) {
+				final Class<?> normalizerCls = this.getNormalizer(file);
+				if (null != normalizerCls) {
+					try {
+						UnitNormalizer.testNormalizer(normalizerCls, file);
+						// Common Debug
+						this.debugOutput(flag, file, true);
+					} catch (AbstractException ex) {
+						if (ex instanceof NormalizerException) {
+							// Error Debug
+							this.debugOutput(flag, file, false);
+							errors.add((NormalizerException) ex);
+						}
+					}
+				}
+			}
+		}
+		return errors;
+	}
+
 	@Test
-	public void testSucTicket() throws AbstractException{
-		UnitNormalizer.testNormalizer(TicketNormalizer.class, "ticket-key-success");
+	public void testFailure() throws AbstractException {
+		final List<NormalizerException> errors = this.execute("failure");
+		final int errorCount = this.countByFlag("failure");
+		assertEquals(errorCount, errors.size());
 	}
-	// action = /deleteTicket
-	@Test(expected = NormalizerException.class)
-	public void testErrDeleteTicket() throws AbstractException {
-		UnitNormalizer.testNormalizer(DeleteTicketNormalizer.class, "deleteTicket-key-failure");
-	}
+
 	@Test
-	public void testSucDeleteTicket() throws AbstractException{
-		UnitNormalizer.testNormalizer(DeleteTicketNormalizer.class, "deleteTicket-key-success");
-	}
-	// action = /tickets
-	@Test(expected = NormalizerException.class)
-	public void testErrTickets() throws AbstractException{
-		UnitNormalizer.testNormalizer(TicketsNormalizer.class, "tickets-status-failure");
-	}
-	@Test
-	public void testSucTickets() throws AbstractException{
-		UnitNormalizer.testNormalizer(TicketsNormalizer.class, "tickets-nostatus-success");
+	public void testSuccess() throws AbstractException {
+		final List<NormalizerException> errors = this.execute("success");
+		assertEquals(Defaults.ZERO, errors.size());
 	}
 }
